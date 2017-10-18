a8c86a164bb2ebe2c1cf8d8b574681c3ae3631a5
Parv Kumar <parvathikumar92@gmail.com>
2017-10-18 18:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Code your solutions in this file
 
-function printBadges(names){
-
- for (let counter = 0, counter < names.length, counter++ ){
-   console.log(`Welcome ${name}! You are employee #${counter+1}.`);
- }
- return names;
+function printBadges(){ 
 
 }
